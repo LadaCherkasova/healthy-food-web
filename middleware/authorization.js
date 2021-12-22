4197962d8c99706a6da75bd225a7d54bb00dc120
Lada Cherkasova <ladacherkasovav@yandex.ru>
2021-12-22 13:32:13 +0300

    change public key to necessary

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -9,8 +9,23 @@ module.exports = async(req, res, next) => {
       return res.status(403).send("Not Authorize");
     }
 
-    const payload = jwt.verify(jwtToken, process.env.healthy_food_secret.replace(/\\n/gm, '\n'),
-      { algorithms: ['RS256']});
+    const key = '-----BEGIN PUBLIC KEY-----\n' +
+      'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA+7z3gV/FrMw3Ap+ipsZ9\n' +
+      'PvJGzUjosr/EGMqd85N7PnpmS7IKUvH5yXdBn2EVZtLJUq0/ujNJ6PdFYp5LlO2o\n' +
+      'AIqI1tOJSvZdXHU4YjWo0g2SN/ZllK7T7hVzX3HR35i29u+q/gzW3whK39nPh0bT\n' +
+      'jr/GNPJb35qw4SGUQzcWELXZ1ejDYWQTwLy1ueDnFCde6BTyVneRhARkXmPD1b57\n' +
+      'dkm1w0ySDaJXmw45V3WuUkXGn6HOAFOwstFjFUYWRqnr6qknKY6UsFa7W+6NfHnt\n' +
+      'GtZj/rY3EqPFlnvNtNK5VDmWZBnSxddpQVoCq1ln2eC89lCBBg9VX4hVpx6TfWiz\n' +
+      'CK4B8hukHox2Q7W2Xp7Ayr5Oc60no+K5eyDE0o5/MLEEi7KLhDYZzzBUYBHxE+fD\n' +
+      'Hw1s8oGr+uYW9DoXr4T2QYjE+rxt81xExCThEHygexACL/D9HZfjHQHF7svkenWa\n' +
+      'lvb2y/0QlIybsfvM1ULLwtOPXrBjte0CUGiA4LguHBJvox6DVN8tI2z8eBgRTnO/\n' +
+      'QhNM1pnvzhrBg45JK3p+rxJohUraS/3HnccnDUfRuqj7eCJRBY5888b7rOsbmOCR\n' +
+      'VJNg38A88k7YCF+1JnkEjAdkNTdsMTVaOMdPRFYMUeVUbYguK38qV4EUnL3lsrNb\n' +
+      'H1qA/mThwRwBFKuoyCgnxacCAwEAAQ==\n' +
+      '-----END PUBLIC KEY-----';
+
+    //process.env.healthy_food_secret.replace(/\\n/gm, '\n')
+    const payload = jwt.verify(jwtToken, key, { algorithms: ['RS256']});
     req.user = payload.user;
 
   } catch(error) {
